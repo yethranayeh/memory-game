949f552640e0cc4839ae12ed9012970838bde594
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2022-02-18 19:11:31 +0300

    added language support files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 /** @format */
 
 import "./styles/App.css";
+import english from "./lang/en.json";
+import turkish from "./lang/tr.json";
+import React, { useState } from "react";
 import Scoreboard from "./components/Scoreboard";
 import Card from "./components/Card";
 
 function App() {
+	const [score, setScore] = useState(0);
+	const [highScore, setHighScore] = useState(0);
+	const [lang, setLang] = useState(english);
+
+	const handleLanguageChange = (e) => {
+		if (e.target.value === "en") {
+			setLang(english);
+		} else if (e.target.value === "tr") {
+			setLang(turkish);
+		}
+	};
+
 	return (
 		<div className='App'>
 			<nav>
-				<Scoreboard />
+				<Scoreboard strings={lang.scoreboard} />
 			</nav>
 			<Card />
 		</div>
